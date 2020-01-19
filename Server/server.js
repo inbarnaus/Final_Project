a635a143818d2aa3d0bd4966b910e6d3d18dc774
itays <shklari@post.bgu.ac.il>
2020-01-19 17:44:22 +0200

    aaa

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const data = require('./Stubs/Dal_Stub');
 const express = require('express');
 const bodyParser = require('body-parser');
-const apartments
+//const apartments
 const app = express();
 const port = 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
