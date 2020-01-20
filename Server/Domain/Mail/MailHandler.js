83437d0a61e8dae0fabd672c82c7510e0ba2c085
inbarnaus <inbarnaus1@gmail.com>
2020-01-20 21:17:48 +0200

    mail with non working timer

--- Server/Domain/Mail/MailHandler.js
+++ Server/Domain/Mail/MailHandler.js
@@ -1,4 +1,5 @@
 var nodemailer = require('nodemailer');
+var CronJob = require('cron').CronJob;
 
 var transporter = nodemailer.createTransport({
   host: 'amtp.gmail.com',
@@ -15,14 +16,14 @@ var transporter = nodemailer.createTransport({
 var mailOptions = {
   from: 'mnh.officesystem@gmail.com',
   to: 'inbarnaus1@gmail.com',
-  subject: 'Sending Email using Node.js',
+  subject: 'עדכון יומי- דיווחים שטרם דווחו',
   text: 'That was easy!'
 };
 
 transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
 });
