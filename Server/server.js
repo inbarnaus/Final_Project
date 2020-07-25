b403481b7c9ccc1d501ce1b69fce6e56059d26c5
itays <shklari@post.bgu.ac.il>
2020-07-25 14:04:40 +0300

    p

--- Server/server.js
+++ Server/server.js
@@ -26,9 +26,9 @@ app.use(cors());
 console.log(process.env.PORT);
 
 const favicon = require('express-favicon');
-app.use(express.static(__dirname + 'Client/build'));
+app.use(express.static(__dirname + '../Client/build'));
 app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'Client/build', 'index.html');
+    const index = path.join(__dirname, '../Client/build', 'index.html');
     res.sendFile(index);
   });
 
