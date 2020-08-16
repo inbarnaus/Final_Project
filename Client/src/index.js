6be29fbfc773cea63301dc08b46e3112fe8ff386
itays <shklari@post.bgu.ac.il>
2020-08-16 20:58:35 +0300

    index to sec

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/App';
-import * as serviceWorker from '../src/serviceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from 'react-router-dom';
 
