4baca06e03b677a80b293710a7166b2d2df732da
itays <shklari@post.bgu.ac.il>
2020-08-18 03:21:52 +0300

    client/ npm run build

---
+++ Client/build/precache-manifest.66f60876d1b9847df157dc4a62fa1292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8aedeab5b9ed577ec3f79925ba15989",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa08a64c689768c3589f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "10a0ade0753c87e59d0d",
+    "url": "/static/css/main.94539087.chunk.css"
+  },
+  {
+    "revision": "fa08a64c689768c3589f",
+    "url": "/static/js/2.c4002d36.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.c4002d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a0ade0753c87e59d0d",
+    "url": "/static/js/main.9f86326f.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
