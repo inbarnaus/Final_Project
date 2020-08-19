027a250281e1653068dedbb5ffab8aa5d16d2600
itays <shklari@post.bgu.ac.il>
2020-08-19 14:23:21 +0300

    k

---
+++ Client/build/precache-manifest.56ae1deea4a8fa244dd334ffad64101a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e8c4bfe3b34a0e8bba8b802c3a11a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa08a64c689768c3589f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da0345206d7d4faa6495",
+    "url": "/static/css/main.94539087.chunk.css"
+  },
+  {
+    "revision": "fa08a64c689768c3589f",
+    "url": "/static/js/2.c4002d36.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.c4002d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0345206d7d4faa6495",
+    "url": "/static/js/main.447f1cfc.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
