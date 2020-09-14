bde1834c0d36512fafb31fb24732c405e82b0d6b
itays <shklari@post.bgu.ac.il>
2020-09-14 19:45:03 +0300

    static last

--- server.js
+++ server.js
@@ -30,15 +30,6 @@ console.log(process.env.PORT);
 
 console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('Client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
-    })
-}
-
 app.post('/login',
     async (req, res) => {
         if(!req.body['email'] || !req.body['password'])
@@ -388,6 +379,15 @@ app.post('/test', async(req, res) => {
     console.log('avabash');
     res.send({avabash: "avabash"});
 });
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('Client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
+    })
+}
 
 app.listen(app.get('port'), () => {console.log(`Example app listening on port ${port}!`)});
 
